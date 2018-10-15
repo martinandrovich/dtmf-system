fa98fd130cc9df91562f9775b1ac60c9484cf165
Martin Androvich <martinandrovich@gmail.com>
2018-10-15 20:52:56 +0200

    Update Constants

--- dtmf-lib/src/signal/constants.h
+++ dtmf-lib/src/signal/constants.h
@@ -2,9 +2,10 @@
 
 // _ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-constexpr auto PI				= 3.141592653589732;
-constexpr auto DURATION			= 500;							// Default signal duration in ms
-constexpr auto PAUSE			= 20;							// Default signal pause in ms
-constexpr auto FADE				= 0.15f;						// Default signal fade percentage in decimal (0.1 = 10%)
-constexpr auto SAMPLE_RATE		= 44100;						// Default signal sample rate in Hz
-constexpr auto AMPLITUDE_MAX	= 16384;						// Maximum possible signal amplitude (100%) (SIGNED INT16 -> 2^15 / 2)
+constexpr auto PI					= 3.141592653589732;
+constexpr auto DURATION				= 500;							// Default signal duration [ms]
+constexpr auto PAUSE				= 20;							// Default signal pause [ms]
+constexpr auto FADE					= 0.15f;						// Default signal fade percentage [decimal] (0.1 = 10%)
+constexpr auto SAMPLE_RATE			= 44100;						// Default signal sample rate in [Hz]
+constexpr auto SAMPLE_INTERVAL		= 50;							// Default interval of sample processing [ms]
+constexpr auto AMPLITUDE_MAX		= 16384;						// Maximum possible signal amplitude (100%) (SIGNED INT16 -> 2^15 / 2)
