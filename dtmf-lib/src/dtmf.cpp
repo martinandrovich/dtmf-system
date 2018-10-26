b5b66d7d98e5e2237dba9a41a711e1355eb94a7e
Martin Androvich <martinandrovich@gmail.com>
2018-10-26 16:42:27 +0200

    Implement DSP

--- dtmf-lib/src/dtmf.cpp
+++ dtmf-lib/src/dtmf.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+
+#define WINVER 0x0500
 #include <Windows.h>
 
 #include "dtmf.h"
@@ -25,9 +27,26 @@ void dtmf::testGeneratorSequence()
 	generator::playbackSequence(test, 50);
 }
 
-void logPayload(std::bitset<3> payload)
+void logPayload(uint toneID)
 {
-	std::cout << "[PAYLOAD] " << payload.to_string() << std::endl;
+	INPUT ip;
+
+	// Set up a generic keyboard event.
+	ip.type = INPUT_KEYBOARD;
+	ip.ki.wScan = 0; // hardware scan code for key
+	ip.ki.time = 0;
+	ip.ki.dwExtraInfo = 0;
+
+	// Press the "A" key
+	ip.ki.wVk = 0x41; // virtual-key code for the "a" key
+	ip.ki.dwFlags = 0; // 0 for key press
+	SendInput(1, &ip, sizeof(INPUT));
+
+	// Release the "A" key
+	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	SendInput(1, &ip, sizeof(INPUT));
+
+	std::cout << "[PAYLOAD]: " << toneID << std::endl;
 }
 
 void dtmf::testDecodeSample()
