e27b490f2d594fcf7e7d5edddb21290f2a1aa455
Martin Androvich <martinandrovich@gmail.com>
2018-11-02 11:21:22 +0100

    Update node

--- dtmf-lib/src/node/node.h
+++ dtmf-lib/src/node/node.h
@@ -2,14 +2,36 @@
 
 class node
 {
-	//// Public Declarations [Interface] //////////////////////////////////////////////////////////////////////////////////////////////
+//// Public Declarations [Interface] //////////////////////////////////////////////////////////////////////////////////////////////
 public:
 
 	// Constructor & Destructor
 	node();
 	virtual ~node();
 
-	//// Private Declarations /////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Constructs
+	struct Action
+	{
+		int clientID;
+		enum actions {null, up, down, left, right, primary, secondary, menu };
+	};
+
+	struct Message
+	{
+		Message(int address, int command)			: direction(address / 8),	id(address & 7),	address(address),				command(command) {};
+		Message(int direction, int id, int command) : direction(direction),		id(id),				address(direction * 8 + id),	command(command) {};
+
+		int command;
+		int direction;
+		int id;
+		int address;
+	};
+
+	// Methods
+	void action(Action::actions action);
+
+//// Private Declarations /////////////////////////////////////////////////////////////////////////////////////////////////////////
 private:
-	;
+	void send(int msg); // msg struct
+	void(*recieved)(int toneID);
 };
