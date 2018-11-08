a2f22ea1552c1f9f12cad9a0521b2e4faa42dc7a
DanielRJohansen <44083218+DanielRJohansen@users.noreply.github.com>
2018-11-08 12:16:31 +0100

    Aner ikke hvad jeg laver

--- console-app/src/main.cpp
+++ console-app/src/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include <dtmf.h>
+#define LOG(x) std::cout << x << std::endl;
 
+void init() {
+	char ID;
+	std::cout << "Er jeg server eller klient? [SPACE]/g \n";
+	char sok = std::getchar();
+	if (sok == ' ') {
+		std::cout << "Server valgt \n";
+		ID = 'S';
+	}
+	else {
+		std::cout << "Klient valgt \n";
+		ID = 'K';
+	}
+
+	switch (ID)
+	{
+	case 'S': {
+		char asd;
+		std::cout << "Vil du tilkoble klienter, y/n? \n";
+			std::cin >> asd;
+		if (asd == 'y') {
+			dtmf::testGenerator();
+		}
+	}
+	case 'K': {
+		while (true)
+			return;
+	}
+	}
+}
 int main()
 {	
-	
-	std::cout << "Ear Rape Simulator 1.0\n";
 
-	std::cout << "Testing Decoder w/ keypresses\n";
-	dtmf::testDecodeKeyboard();
+	init();
 	
 	std::cin.get();
 	return 0;
