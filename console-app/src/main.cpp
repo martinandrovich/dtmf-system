50714785fbb81aa4bbda85042981ff689fa0c090
MagnusHutters <43892387+MagnusHutters@users.noreply.github.com>
2018-11-10 00:56:59 +0100

    fixup

--- console-app/src/main.cpp
+++ console-app/src/main.cpp
@@ -7,12 +7,9 @@
 void actionRecieved(int action, int id) {
 
 }
-<<<<<<< HEAD
 
 #include <dtmf/node.h>
 #include <dtmf/toolbox.h>
-=======
->>>>>>> f1ae74b4d136d5edb6245a60dc70985fba6a99bf
 
 int main()
 {	
@@ -27,7 +24,6 @@ int main()
 
 
 	//dtmf::testDecodeKeyboard();
-<<<<<<< HEAD
 
 	// init
 	std::cout << "Ear Rape Simulator 1.1\n";
@@ -39,15 +35,10 @@ int main()
 	std::vector<short> test = { 0, 2, 3, -4, 2, 1, -7 };
 	dtmf::toolbox::plotSamples(test);
 
-=======
->>>>>>> f1ae74b4d136d5edb6245a60dc70985fba6a99bf
 
 	std::cin.get();
 	return 0;
-<<<<<<< HEAD
 
-=======
->>>>>>> f1ae74b4d136d5edb6245a60dc70985fba6a99bf
 
 }
 
