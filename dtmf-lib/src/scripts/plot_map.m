c5f8e11883b5dc67c40782970e6c2309475cb633
Martin Androvich <martinandrovich@gmail.com>
2018-11-13 23:47:01 +0100

    Update MATLAB scripts

--- dtmf-lib/src/scripts/plot_map.m
+++ dtmf-lib/src/scripts/plot_map.m
@@ -1,20 +1,28 @@
-function [] = plot_map(filename)
+function [] = plot_map(filename, name, x, y)
 
     % Usage
     % plot_script("test.dat")
     
+    % Set figure title
+    figure('Name', name, 'NumberTitle', 'off');
+    
     % Print information
-    disp('MATLAB Map Plot Script 1.0.0');
+    disp('MATLAB Map Plot Script 1.1.0');
 
     % Load data
     data = load(filename);
 
     % Plot
     plot(data(:,1), data(:,2))
-
+    
+   % Center Y-Axis
+    ymax = max(abs(data(:, 2)));
+    ymax = ymax + 0.1*ymax;
+    ylim([-ymax, ymax]);
+    
     % Labels
-    xlabel('Element Number');
-    ylabel('Magnitude');
-    title('Map Plot');
+    title(name);
+    xlabel(x);
+    ylabel(y);
 
 end
--- dtmf-lib/src/scripts/plot_samples.m
+++ dtmf-lib/src/scripts/plot_samples.m
@@ -1,25 +1,25 @@
-function [] = plot_map(filename, name, x, y)
+function [] = plot_samples(filename, name, x, y)
 
     % Usage
-    % plot_script("test.dat")
+    % plot_script("test.dat", "title", "x", "y")
     
     % Set figure title
     figure('Name', name, 'NumberTitle', 'off');
     
     % Print information
-    disp('MATLAB Map Plot Script 1.1.0');
+    disp('MATLAB Samples Plot Script 1.1.0');
 
     % Load data
     data = load(filename);
 
     % Plot
-    plot(data(:,1), data(:,2))
+    plot(data);
     
-   % Center Y-Axis
-    ymax = max(abs(data(:, 2)));
+    % Center Y-Axis
+    ymax = max(abs(data(:)));
     ymax = ymax + 0.1*ymax;
     ylim([-ymax, ymax]);
-    
+
     % Labels
     title(name);
     xlabel(x);
