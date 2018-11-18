e083945f10559393d774f9a60366a6560bd8beaf
Martin Androvich <martinandrovich@gmail.com>
2018-11-18 19:07:21 +0100

    Update commands

--- console-app/src/main.cpp
+++ console-app/src/main.cpp
@@ -10,16 +10,18 @@
 // commands map
 std::unordered_map<std::string, void(*)(std::string args)> commandMap = 
 {
-	{ "init",	[](std::string args) { initSystem(args);							} },
-	{ "help",	[](std::string args) { help(args);									} },
-	{ "exit",	[](std::string args) { exit(0);										} },
-	{ "cls",	[](std::string args) { initCLI();									} },
-	{ "log",	[](std::string args) { dtmf::toolbox::testDecoderLog();				} },
-	{ "seq",	[](std::string args) { dtmf::toolbox::testGeneratorSequence();		} },
-	{ "lat",	[](std::string args) { dtmf::toolbox::testLatency();				} },
-	{ "step",	[](std::string args) { dtmf::toolbox::testStepWindow2(args);		} },
-	{ "sam",	[](std::string args) { dtmf::toolbox::testSampler2();				} },
-	{ "gor",	[](std::string args) { dtmf::toolbox::testGeneratorGoertzel(args);	} },
+	{ "init",	[](std::string args) { initSystem(args);													} },
+	{ "help",	[](std::string args) { help(args);															} },
+	{ "exit",	[](std::string args) { exit(0);																} },
+	{ "cls",	[](std::string args) { initCLI();															} },
+	{ "log",	[](std::string args) { dtmf::toolbox::logDecoder();											} },
+	{ "seq",	[](std::string args) { dtmf::toolbox::playbackSequence(args);								} },
+	{ "glog",	[](std::string args) { dtmf::toolbox::logGoertzel(args);									} },
+	{ "glogt",	[](std::string args) { auto glog = new std::thread(dtmf::toolbox::logGoertzel, args);		} },
+	{ "lat",	[](std::string args) { dtmf::toolbox::testLatency();										} },
+	{ "step",	[](std::string args) { dtmf::toolbox::testStepWindow2(args);								} },
+	{ "sam",	[](std::string args) { dtmf::toolbox::testSampler2();										} },
+	{ "gor",	[](std::string args) { dtmf::toolbox::testGeneratorGoertzel(args);							} },
 };
 
 // main function
