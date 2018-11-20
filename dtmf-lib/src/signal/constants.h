0eddc723c88f62cd90a198001754dda927627743
Martin Androvich <martinandrovich@gmail.com>
2018-11-20 21:04:12 +0100

    Update constants

--- dtmf-lib/src/signal/constants.h
+++ dtmf-lib/src/signal/constants.h
@@ -9,8 +9,9 @@ constexpr auto 	DATA_PATH			= "dat/";										// Path of data directory (defaul
 constexpr auto	PI					= 3.141592653589732;
 constexpr auto	AMPLITUDE_MAX		= 16384;										// Maximum possible signal amplitude (100%) (SIGNED INT16 -> 2^15 / 2)
 constexpr auto	DURATION			= 50;											// Default signal duration [ms]
-constexpr auto	PAUSE				= 20;											// Default signal pause [ms]
-constexpr int	DEBOUNCE			= (DURATION + PAUSE) * 1;						// Default debounce value [ms] (default = 90% of DURATION + PAUSE)
+constexpr auto	PAUSE				= 50;											// Default signal pause [ms]
+constexpr int	DEBOUNCE			= (DURATION + 20) * 1;							// Default debounce value [ms] (default = 90% of DURATION + PAUSE)
+constexpr int	LATENCY				= 100;											// It's the fucking latency, bitch
 constexpr auto	FADE				= 0.10f;										// Default signal fade percentage [decimal] (0.1 = 10%)
 constexpr auto	SAMPLE_RATE			= 88200;										// Default signal sample rate in [Hz] (default = 44100)
 constexpr auto	SAMPLE_INTERVAL		= 10;											// Default interval of sample processing [ms]
@@ -21,4 +22,4 @@ constexpr int 	NUMPTS				= SAMPLE_RATE * (SAMPLE_INTERVAL / 1000.f);		// Number 
 //const int		freqHigh[4]			= { 1209, 1336, 1477, 1633 };
 const int		freq[8]				= { 697,  770,  852,  941, 1209, 1336, 1477, 1633 };
 const int		N[8]				= { 441,  403,  267,  441,  294,  331,  441,  441 };
-const int		freqThresholds[8]	= { 1000,  1000,  1000,  1000,  1000,  1000,  1000,  1000 };
+const int		freqThresholds[8]	= { 50,  50,  50,  50,  100,  100,  100,  100 };
