4faa4152f5d7ebfc524fb5c5f15f26be05517de2
Martin Androvich <martinandrovich@gmail.com>
2018-11-23 15:19:11 +0100

    Update constants

--- dtmf-lib/src/signal/constants.h
+++ dtmf-lib/src/signal/constants.h
@@ -10,12 +10,14 @@ constexpr auto	PI					= 3.141592653589732;
 constexpr auto	AMPLITUDE_MAX		= 16384;										// Maximum possible signal amplitude (100%) (SIGNED INT16 -> 2^15 / 2)
 constexpr auto	DURATION			= 50;											// Default signal duration [ms]
 constexpr auto	PAUSE				= 50;											// Default signal pause [ms]
-constexpr int	DEBOUNCE			= (DURATION + 20) * 1;							// Default debounce value [ms] (default = 90% of DURATION + PAUSE)
-constexpr int	TIMEOUT				= 500;											// ...
+constexpr int	DEBOUNCE			= (DURATION + 40) * 1;							// Default debounce value [ms] (default = 90% of DURATION + PAUSE)
+constexpr int	DEBOUNCE2			= DURATION * 2;									// Default debounce value [ms] (default = DURATION)
+constexpr int	TH_MULTIPLIER		= 10;											// Threshold multiplier
+constexpr int	TIMEOUT				= 500;											// State Machine Protocol
 constexpr int	LATENCY				= 100;											// It's the fucking latency, bitch
 constexpr int	LATENCY_BUFFER		= 20;											// It's the fucking latency buffer, bitch
 constexpr auto	FADE				= 0.10f;										// Default signal fade percentage [decimal] (0.1 = 10%)
-constexpr auto	SAMPLE_RATE			= 88200;										// Default signal sample rate in [Hz] (default = 44100)
+constexpr auto	SAMPLE_RATE			= 44100;										// Default signal sample rate in [Hz] (default = 44100)
 constexpr auto	SAMPLE_INTERVAL		= 10;											// Default interval of sample processing [ms]
 constexpr auto	STEP_WINDOW_SIZE	= 5;											// Number of minimum samples in queue (signal duration / sample interval)
 constexpr int 	NUMPTS				= SAMPLE_RATE * (SAMPLE_INTERVAL / 1000.f);		// Number of samples (size of array for sampler2)
@@ -23,5 +25,4 @@ constexpr int 	NUMPTS				= SAMPLE_RATE * (SAMPLE_INTERVAL / 1000.f);		// Number 
 //const int		freqLow[4]			= {  697,  770,  852,  941 };
 //const int		freqHigh[4]			= { 1209, 1336, 1477, 1633 };
 const int		freq[8]				= { 697,  770,  852,  941, 1209, 1336, 1477, 1633 };
-const int		N[8]				= { 441,  403,  267,  441,  294,  331,  441,  441 };
 const int		freqThresholds[8]	= { 50,  50,  50,  50,  100,  100,  100,  100 };
